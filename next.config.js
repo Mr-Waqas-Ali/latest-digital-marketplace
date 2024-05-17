6d15626814c7c478879a292ac32e1b86055726e5
Waqas Ali <143681231+Mr-Waqas-Ali@users.noreply.github.com>
2024-05-18 02:32:31 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		remotePatterns: [
-			{
-				protocol: "http",
-				hostname: "localhost",
-			},
-			{
-				protocol: "https",
-				hostname: "latest-digital-marketplace.vercel.app",
-			},
+		domains: [
+			'localhost',
+			'latest-digital-marketplace.vercel.app',
 		],
 	},
-};
+}
 
 module.exports = nextConfig;
